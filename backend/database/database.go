808629f36a90eb08f95e4813415a5259892abfda
ExMe4 <joao.f.regateiro@gmail.com>
2025-05-19 18:33:24 +0200

    [backend] postgres db setup

---
+++ backend/database/database.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+var DB *pgx.Conn
+
+func Connect() {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	conn, err := pgx.Connect(ctx, dsn)
+	if err != nil {
+		log.Fatal("Unable to connect to database: ", err)
+	}
+
+	DB = conn
+	fmt.Println("✅ Connected to PostgreSQL")
+}

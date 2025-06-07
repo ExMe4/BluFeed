32786278e5f9f18e560d3bfe78c5bd2d740d649d
ExMe4 <joao.f.regateiro@gmail.com>
2025-06-07 14:30:27 +0200

    [frontend] added env files and fix glogin

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class User {
   final String id;
@@ -13,22 +14,34 @@ class AuthService {
   User? _user;
 
   Future<bool> signInWithGoogle() async {
-    final GoogleSignIn _googleSignIn = GoogleSignIn();
+    final GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: ['email'],
+      serverClientId: dotenv.env['SERVER_CLIENT_ID'],
+    );
 
     try {
       final account = await _googleSignIn.signIn();
-      if (account == null) return false;
+
+      if (account == null) {
+        print("User cancelled Google Sign-In");
+        return false;
+      }
 
       final authentication = await account.authentication;
       final idToken = authentication.idToken;
       if (idToken == null) return false;
 
+      print("ID Token: $idToken");
+
       // Sends the token to the backend
       final response = await http.post(
-        Uri.parse("http://localhost:8080/api/auth/google"),
+        Uri.parse("http://192.168.178.28:3000/api/auth/google"),
         headers: {"Content-Type": "application/json"},
         body: jsonEncode({"token": idToken}),
       );
+
+      print("Status: ${response.statusCode}");
+      print("Body: ${response.body}");
 
       if (response.statusCode != 200) return false;
 
@@ -37,6 +50,11 @@ class AuthService {
       return true;
     } catch (e) {
       print("Google Sign-In failed: $e");
+
+      if (e is http.ClientException) {
+        print("ClientException details: ${e.message}");
+      }
+
       return false;
     }
   }
